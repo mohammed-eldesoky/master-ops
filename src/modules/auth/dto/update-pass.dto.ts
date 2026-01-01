2f4e8cfc79599efd557bc1f41afbecfdf87444ab
mohamed <mohammedeldesoky77@gmail.com>
2026-01-01 16:37:46 +0200

    implement Update Password Dto with validation

---
+++ src/modules/auth/dto/update-pass.dto.ts
@@ -0,0 +1,18 @@
+import { IsNotEmpty, IsString, MinLength, Matches } from 'class-validator';
+
+export class UpdatePassDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+$/, {
+    message: 'Password must contain at least one letter and one number',
+  })
+  oldPassword: string;
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+$/, {
+    message: 'Password must contain at least one letter and one number',
+  })
+  newPassword: string;
+}
