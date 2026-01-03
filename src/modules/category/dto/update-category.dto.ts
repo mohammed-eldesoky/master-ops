2b7079ece2d0583cc4790d28463f7f371f2a365a
mohamed <mohammedeldesoky77@gmail.com>
2026-01-03 18:16:29 +0200

    feat : implement update category  dto with validation && update category factory

--- src/modules/category/dto/update-category.dto.ts
+++ src/modules/category/dto/update-category.dto.ts
@@ -1,3 +1,25 @@
+import {
+  IsNotEmpty,
+  IsObject,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
 
+export class UpdateCategoryDto {
+  @IsString()
+  @IsOptional()
+  @MinLength(2, { message: 'Name must be at least 2 characters long' })
+  name: string;
 
-export class UpdateCategoryDto {}
+  @IsString()
+  @IsOptional()
+  description: string;
+
+  @IsObject()
+  @IsOptional()
+  logo: {
+    secure_url: string;
+    public_id: string;
+  };
+}
--- src/modules/category/factory/category.factory.ts
+++ src/modules/category/factory/category.factory.ts
@@ -1,11 +1,13 @@
 import slugify from 'slugify';
 import { CreateCategoryDto } from '../dto/create-category.dto';
 import { Category } from '../entities/category.entity';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { UpdateCategoryDto } from '../dto/update-category.dto';
+import { CategortyRepository } from 'src/models';
 
 @Injectable()
 export class CategoryFactory {
-  constructor() {}
+  constructor(private readonly categoryRepo: CategortyRepository) {}
 
   createCategory(createCategory: CreateCategoryDto, user: any) {
     const category = new Category();
@@ -20,6 +22,34 @@ export class CategoryFactory {
     category.updatedBy = user._id;
     category.isActive = true;
     category.logo = createCategory.logo;
+    return category;
+  }
+
+  async updateCategory(
+    id: string,
+    updateCategory: UpdateCategoryDto,
+    user: any,
+  ) {
+    //check if category exist
+    const categoryExist = await this.categoryRepo.getOne({ _id: id });
+    //fail case
+    if (!categoryExist) {
+      throw new NotFoundException('Category does not exist');
+    }
+    const category = new Category();
+    const newName = updateCategory.name || categoryExist.name;
+    const description = updateCategory.description || categoryExist.description;
+    const logo = updateCategory.logo || categoryExist.logo;
+
+    category.name = newName;
+    category.description = description;
+    category.slug = slugify(newName, {
+      replacement: '-',
+      lower: true,
+      trim: true,
+    });
+    category.updatedBy = user._id;
+    category.logo = logo;
     return category;
   }
 }
