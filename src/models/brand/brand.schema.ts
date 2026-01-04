51b47d104ba8002fb36d380e9f456310fc5f6549
mohamed <mohammedeldesoky77@gmail.com>
2026-01-04 14:57:05 +0200

    feat (DB): implement brand schema

---
+++ src/models/brand/brand.schema.ts
@@ -0,0 +1,43 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Types } from 'mongoose';
+
+@Schema({
+  timestamps: true,
+  toJSON: {
+    virtuals: true,
+  },
+  toObject: {
+    virtuals: true,
+  },
+})
+export class Brand {
+  readonly _id: Types.ObjectId;
+
+  @Prop({ type: String, required: true, unique: true, trim: true })
+  name: string;
+
+  @Prop({ type: String, required: false, trim: true })
+  description: string;
+
+  @Prop({ type: String, required: true, unique: true, trim: true })
+  slug: string;
+  @Prop({ type: Types.ObjectId, ref: 'User', required: true })
+  createdBy: Types.ObjectId;
+  @Prop({ type: Types.ObjectId, ref: 'User' })
+  updatedBy: Types.ObjectId;
+  @Prop({ type: Boolean, default: true })
+  isActive: boolean;
+  @Prop({
+    type: {
+      secure_url: { type: String, required: true },
+      public_id: { type: String, required: true },
+    },
+    required: false,
+  })
+  logo: {
+    secure_url: string;
+    public_id: string;
+  };
+}
+
+export const brandSchema = SchemaFactory.createForClass(Brand);
